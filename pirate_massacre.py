5554c168861f78757cc277465d21856890af710b
Guntereno <GunterPete@hotmail.com>
2022-02-01 18:30:14 +0000

    List missions by each system faction

--- pirate_massacre.py
+++ pirate_massacre.py
@@ -1,29 +1,34 @@
 import journal_scan
-import json
 
 class MissionInfo:
     id = None
     giving_faction = None
     target_faction = None
-    kill_count = -1
+    remaining_kills = -1
+    total_kills = -1
+    system_name = None
+    description = None
 
-    def __init__(self, id, giving_faction, target_faction, kill_count):
+    def __init__(self, id, giving_faction, target_faction, kill_count, system_name, description):
         self.id = id
         self.giving_faction = giving_faction
         self.target_faction = target_faction
-        self.kill_count = kill_count
+        self.remaining_kills = self.total_kills = kill_count
+        self.system_name = system_name
+        self.description = description
+class SystemInfo:
+    name = None
+    factions = None
 
-    def dump(self):
-        print(
-            '{id="' + str(self.id) +
-            '", giving_faction="' + str(self.giving_faction) +
-            '", target_faction="' + str(self.target_faction) +
-            '", kill_count="' + str(self.kill_count) + '"}')
+    def __init__(self, name, factions):
+        self.name = name
+        self.factions = factions
 
 class PirateMassacreScanner(journal_scan.JournalScanner):
-
     mission_queue = []
     mission_dict = {}
+    system_dict = {}
+    current_system = None
 
     def add_mission(self, id, mission):
         self.mission_queue.append(mission)
@@ -51,7 +56,7 @@ class PirateMassacreScanner(journal_scan.JournalScanner):
                     id = mission['MissionID']
                     # We only want to add missions we're not already tracking
                     if not id in self.mission_dict:
-                        self.add_mission(id, MissionInfo(id, None, -1))
+                        self.add_mission(id, MissionInfo(id, None, -1, None))
 
     # {
     #     "timestamp": "2022-01-31T20:29:33Z",
@@ -75,7 +80,14 @@ class PirateMassacreScanner(journal_scan.JournalScanner):
     def handle_mission_accepted(self, event):
         if event['Name'] == 'Mission_MassacreWing':
             id = event['MissionID']
-            self.add_mission(id, MissionInfo(id, event['Faction'], event['TargetFaction'], event['KillCount']))
+            mission =  MissionInfo(
+                id,
+                event['Faction'],
+                event['TargetFaction'],
+                event['KillCount'],
+                self.current_system,
+                event['LocalisedName'])
+            self.add_mission(id, mission)
 
     # {
     #     "timestamp":"2022-01-31T21:07:45Z",
@@ -154,9 +166,134 @@ class PirateMassacreScanner(journal_scan.JournalScanner):
                 continue
             if mission.giving_faction in handled_factions:
                 continue
-            if mission.kill_count > 0:
-                mission.kill_count -= 1
+            if mission.remaining_kills > 0:
+                mission.remaining_kills -= 1
                 handled_factions.add(mission.giving_faction)
+
+    # {
+    #   "timestamp": "2022-01-31T19:52:04Z",
+    #   "event": "Location",
+    #   "Docked": false,
+    #   "Taxi": false,
+    #   "Multicrew": false,
+    #   "StarSystem": "HR 1613",
+    #   "SystemAddress": 697688901979,
+    #   "StarPos": [
+    #     54.3125,
+    #     -68.0625,
+    #     -136.375
+    #   ],
+    #   "SystemAllegiance": "Independent",
+    #   "SystemEconomy": "$economy_Extraction;",
+    #   "SystemEconomy_Localised": "Extraction",
+    #   "SystemSecondEconomy": "$economy_Colony;",
+    #   "SystemSecondEconomy_Localised": "Colony",
+    #   "SystemGovernment": "$government_Confederacy;",
+    #   "SystemGovernment_Localised": "Confederacy",
+    #   "SystemSecurity": "$SYSTEM_SECURITY_low;",
+    #   "SystemSecurity_Localised": "Low Security",
+    #   "Population": 115439,
+    #   "Body": "HR 1613",
+    #   "BodyID": 0,
+    #   "BodyType": "Star",
+    #   "Factions": [
+    #     {
+    #       "Name": "HR 1613 Prison Colony",
+    #       "FactionState": "None",
+    #       "Government": "PrisonColony",
+    #       "Influence": 0.115462,
+    #       "Allegiance": "Independent",
+    #       "Happiness": "$Faction_HappinessBand2;",
+    #       "Happiness_Localised": "Happy",
+    #       "MyReputation": 15
+    #     },
+    #     ...
+    #   ],
+    #   "SystemFaction": {
+    #     "Name": "Brazilian League of Pilots"
+    #   }
+    # }
+    def handle_location(self, event):
+        system_name = event['StarSystem']
+        self.current_system = system_name
+        self.store_system_info(event, system_name)
+
+    # {
+    #   "timestamp": "2022-01-31T20:41:04Z",
+    #   "event": "FSDJump",
+    #   "Taxi": false,
+    #   "Multicrew": false,
+    #   "StarSystem": "HIP 23575",
+    #   "SystemAddress": 800768117083,
+    #   "StarPos": [
+    #     54.21875,
+    #     -66.0625,
+    #     -142.5
+    #   ],
+    #   "SystemAllegiance": "Independent",
+    #   "SystemEconomy": "$economy_Industrial;",
+    #   "SystemEconomy_Localised": "Industrial",
+    #   "SystemSecondEconomy": "$economy_Colony;",
+    #   "SystemSecondEconomy_Localised": "Colony",
+    #   "SystemGovernment": "$government_PrisonColony;",
+    #   "SystemGovernment_Localised": "Prison colony",
+    #   "SystemSecurity": "$SYSTEM_SECURITY_low;",
+    #   "SystemSecurity_Localised": "Low Security",
+    #   "Population": 3481542,
+    #   "Body": "HIP 23575 A",
+    #   "BodyID": 1,
+    #   "BodyType": "Star",
+    #   "JumpDist": 10.207,
+    #   "FuelUsed": 0.547034,
+    #   "FuelLevel": 15.452966,
+    #   "Factions": [
+    #     {
+    #       "Name": "HR 1613 Prison Colony",
+    #       "FactionState": "None",
+    #       "Government": "PrisonColony",
+    #       "Influence": 0.289157,
+    #       "Allegiance": "Independent",
+    #       "Happiness": "$Faction_HappinessBand2;",
+    #       "Happiness_Localised": "Happy",
+    #       "MyReputation": 15,
+    #       "RecoveringStates": [
+    #         {
+    #           "State": "Election",
+    #           "Trend": 0
+    #         }
+    #       ]
+    #     },
+    #     ...
+    #   ],
+    #   "SystemFaction": {
+    #     "Name": "HR 1613 Prison Colony"
+    #   },
+    #   "Conflicts": [
+    #     {
+    #       "WarType": "election",
+    #       "Status": "",
+    #       "Faction1": {
+    #         "Name": "HR 1613 Prison Colony",
+    #         "Stake": "Plucker Orbital",
+    #         "WonDays": 1
+    #       },
+    #       "Faction2": {
+    #         "Name": "Noblemen of Tacare",
+    #         "Stake": "Salgari Colony",
+    #         "WonDays": 0
+    #       }
+    #     }
+    #   ]
+    # }
+    def handle_fsd_jump(self, event):
+        self.handle_location(event)
+
+    def store_system_info(self, event, system_name):
+        if system_name in self.system_dict:
+            return
+        if 'Factions' in event:
+            faction_names = list(map(lambda x: x['Name'], event['Factions']))
+            self.system_dict[system_name] = SystemInfo(self.current_system, faction_names)
 
     def __init__(self):
         self.register_handler("Missions", self.handle_missions)
@@ -165,19 +302,43 @@ class PirateMassacreScanner(journal_scan.JournalScanner):
         self.register_handler("MissionFailed", self.handle_mission_failed)
         self.register_handler("MissionCompleted", self.handle_mission_completed)
         self.register_handler("Bounty", self.handle_bounty)
+        self.register_handler("FSDJump", self.handle_fsd_jump)
+        self.register_handler("Location", self.handle_location)
 
     def finalise(self):
-        output_dict = {}
-        for id in self.mission_dict:
-            mission = self.mission_dict[id]
-            faction = mission.giving_faction
-            if not faction in output_dict:
-                output_dict[faction] = []
-            output_dict[faction].append(mission)
+        system_set = sorted(set(map(lambda x: x.system_name, self.mission_queue)))
 
-        for key in sorted(output_dict.keys()):
-            for mission in output_dict[key]:
-                print(f'{mission.giving_faction}: {mission.kill_count}')
+        mission_dict = {}
+        for mission in self.mission_queue:
+            system_faction = (mission.system_name, mission.giving_faction)
+            if not system_faction in mission_dict:
+                mission_dict[system_faction] = []
+            mission_dict[system_faction].append(mission)
+
+        for system_name in sorted(system_set):
+            print(f'# {system_name}')
+            system_info = self.system_dict[system_name]
+            for faction_name in sorted(system_info.factions):
+                system_faction = (system_name, faction_name)
+                print(f'## {faction_name}')
+                if system_faction in mission_dict:
+                    for mission in mission_dict[system_faction]:
+                        print(f'  - {mission.description}: {mission.remaining_kills}/{mission.total_kills} remaining')
+                else:
+                    print('  - None')
+            print()
+
+        # output_dict = {}
+        # for id in self.mission_dict:
+        #     mission = self.mission_dict[id]
+        #     faction = mission.giving_faction
+        #     if not faction in output_dict:
+        #         output_dict[faction] = []
+        #     output_dict[faction].append(mission)
+
+        # for key in sorted(output_dict.keys()):
+        #     for mission in output_dict[key]:
+        #         print(f'{mission.giving_faction}: {mission.kill_count}')
 
 if __name__ == "__main__":
     journal_scan.scan_journal(PirateMassacreScanner())
